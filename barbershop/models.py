247cc8c7977b41fd5e2b3f1188d9c0d75ef36cde
Nirzi <nirzzii@mail.ru>
2025-04-20 10:09:58 +0500

    hw_<39>: создание класса review

--- barbershop/models.py
+++ barbershop/models.py
@@ -57,3 +57,21 @@ class Order(models.Model):
     class Meta:
         verbose_name = "Заказ"
         verbose_name_plural = "Заказы"
+
+class Review(models.Model):
+    text = models.TextField(verbose_name="Текст отзыва")
+    client_name = models.CharField(max_length=100, blank=True, verbose_name="Имя клиента")
+    master = models.ForeignKey(Master, on_delete=models.CASCADE, verbose_name="Мастер")
+    photo = models.ImageField(upload_to="reviews/", blank=True, null=True, verbose_name="Фотография")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    rating = models.PositiveSmallIntegerField(
+        validators=[MinValueValidator(1), MaxValueValidator(5)], verbose_name="Оценка"
+    )
+    is_published = models.BooleanField(default=True, verbose_name="Опубликован")
+
+    def __str__(self):
+        return f"Отзыв от {self.client_name or 'аноним'} ({self.rating})"
+
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
