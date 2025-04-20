dd330c7542cdcb2cab4047d39e10ee10be0ada3a
Nirzi <nirzzii@mail.ru>
2025-04-20 10:08:52 +0500

    hw_<39>: создание класса service и master

--- barbershop/models.py
+++ barbershop/models.py
@@ -7,3 +7,35 @@ STATUS_CHOICES = [
     ('completed', 'Завершена'),
     ('canceled', 'Отменена'),
 ]
+
+class Service(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Название")
+    description = models.TextField(blank=True, verbose_name="Описание")
+    price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Цена")
+    duration = models.PositiveIntegerField(verbose_name="Длительность (минут)")
+    is_popular = models.BooleanField(default=False, verbose_name="Популярная услуга")
+    image = models.ImageField(upload_to="services/", blank=True, verbose_name="Изображение")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Услуга"
+        verbose_name_plural = "Услуги"
+
+
+class Master(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Имя")
+    photo = models.ImageField(upload_to="masters/", blank=True, verbose_name="Фотография")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    address = models.CharField(max_length=255, verbose_name="Адрес")
+    experience = models.PositiveIntegerField(verbose_name="Стаж работы (лет)")
+    services = models.ManyToManyField(Service, related_name="masters", verbose_name="Услуги")
+    is_active = models.BooleanField(default=True, verbose_name="Активен")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Мастер"
+        verbose_name_plural = "Мастера"
